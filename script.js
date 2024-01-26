6485604ce5ff49e01052fa4dd9a61f0618e2365b
yaswanth kumar <yeswanth2kumar@gmail.com>
2024-01-26 12:48:50 +0530

    Third commit

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function turnOnRed(){
     stopButtonElement.style.backgroundColor = "#cf1124";
     stopLightElement.style.backgroundColor = "#cf1124";
     readyButtonElement.style.backgroundColor = "#1f1d41";
-    readyLightElement.style.backgroundColor = "4b5069";
+    readyLightElement.style.backgroundColor = "#4b5069";
     goButtonElement.style.backgroundColor = "#1f1d41"
-    goLightElement.style.backgroundColor = "4b5069"
+    goLightElement.style.backgroundColor = "#4b5069"
 
 }
 
@@ -22,8 +22,7 @@ function turnOnYellow(){
     stopButtonElement.style.backgroundColor = "#1f1d41";
     stopLightElement.style.backgroundColor = "#4b5069";
     goButtonElement.style.backgroundColor = "#1f1d41";
-    goLightElement.style.backgroundColor = "4b5069";
-
+    goLightElement.style.backgroundColor = "#4b5069";
 
 }
 
@@ -33,7 +32,5 @@ function turnOnGreen(){
     stopButtonElement.style.backgroundColor = "#1f1d41";
     stopLightElement.style.backgroundColor = "#4b5069";
     readyButtonElement.style.backgroundColor = "#1f1d41";
-    readyLightElement.style.backgroundColor = "4b5069";
-
-
+    readyLightElement.style.backgroundColor = "#4b5069";
 }
